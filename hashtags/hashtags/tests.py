e37d675dd7dcc00559b28e0c7bcc0529e3eb315f
wikibootup <wikibootup@gmail.com>
2016-05-27 21:36:49 +0900

    add basic home html test & view

--- hashtags/hashtags/tests.py
+++ hashtags/hashtags/tests.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 
 import hashtags.views
 
@@ -9,3 +10,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_view(self):
         found = resolve('/')
         self.assertEqual(found.func, hashtags.views.home)
+
+    def test_home_returns_correct_html(self):
+        request = HttpRequest()
+        response = hashtags.views.home(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>hashtags</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
--- hashtags/hashtags/views.py
+++ hashtags/hashtags/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("<title>Hashtags</title>")
+    return render(request, 'home.html')
