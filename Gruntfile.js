534dfd1b59a5e97b71a43e75b87cb4a53146fd11
wikibootup <wikibootup@gmail.com>
2016-05-28 21:35:08 +0900

    remove uglify js test in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! test_uglify <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'test_uglify.js',
-        dest: 'test_uglify.min.js'
-      }
-    }
   });
-
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
