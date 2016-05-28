ed31c0853651ef4513c2e54c49210e0c5a275893
wikibootup <wikibootup@gmail.com>
2016-05-28 21:53:05 +0900

    simple phantom qunit test in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,21 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    qunit: {
+      files: ['**/tests/**/*.html']
+    },
+
+    watch: {
+      tasks: ['qunit']
+    }
   });
 
+  grunt.registerTask('test', ['qunit']);
+
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', []);
 
 };
