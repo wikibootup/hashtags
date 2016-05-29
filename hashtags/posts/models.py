3524d442cb54ec06825de99109a78f3296a61557
wikibootup <wikibootup@gmail.com>
2016-05-29 14:38:21 +0900

    add basic tag, post model

--- hashtags/posts/models.py
+++ hashtags/posts/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 
 class Post(models.Model):
-    pass
+    text = models.TextField(blank=True, null=True)
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=150, unique=True)
+    post = models.ManyToManyField(Post)
