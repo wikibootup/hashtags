fccb6c2dcedd1505f623fc93cf1ec5f1df987d8a
wikibootup <wikibootup@gmail.com>
2016-05-29 14:37:45 +0900

    unittest post tag m to m create

--- hashtags/posts/tests.py
+++ hashtags/posts/tests.py
@@ -1,13 +1,21 @@
 from django.test import TestCase
 
-from posts.models import Post
+from posts.models import Post, Tag
 
 
 class PostTest(TestCase):
-    
+
     def test_post_contains_correct_text_and_tags(self):
-        post = Post.create_object(text='It is text', tags=('tag1', 'tag2'))
+        post = Post(text='It is text')
+        post.save()
+        tag1 = Tag(tag='tag1')
+        tag1.save()
+        tag2 = Tag(tag='tag2')
+        tag2.save()
+        
+        tag1.post.add(post)
+        tag2.post.add(post)
 
         self.assertEqual('It is text', post.text)
-        self.assertContains('tag1', post.tags)
-        self.assertContains('tag2', post.tags)
+        self.assertTrue(post.tag_set.all().get(tag='tag1'))
+        self.assertTrue(post.tag_set.all().get(tag='tag2'))
