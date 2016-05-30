ce070b84857e668db76f775f9fad4915f13dd465
wikibootup <wikibootup@gmail.com>
2016-05-30 22:30:41 +0900

    basic search form in home view

--- hashtags/hashtags/views.py
+++ hashtags/hashtags/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 
-from posts.models import Post
+from posts.models import Post, Tag
 from posts.forms import SearchForm
 
 
 def home(request):
+    if request.method == 'GET':
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            tag_query = form.cleaned_data['search_tags']
+            tag = Tag.objects.get(tag=tag_query)
+            posts = tag.post.all()
+
+            return render(request, 'posts.html', {
+                'posts': posts,
+                'form': SearchForm()
+            })
+
     return render(request, 'home.html', {
         'posts': Post.objects.all(),
         'form': SearchForm()
