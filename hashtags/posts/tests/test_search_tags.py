ca735062b3e56da06953017d21360c18d7491f75
wikibootup <wikibootup@gmail.com>
2016-05-30 17:24:18 +0900

    UT roughly search tags query result

--- hashtags/posts/tests/test_search_tags.py
+++ hashtags/posts/tests/test_search_tags.py
@@ -10,3 +10,27 @@ class SearchTagsTest(TestCase):
     def test_form_renders_search_tags_input(self):
         form = SearchForm()
         self.assertIn('placeholder="검색"', form.as_p())
+
+    def test_search_tags_query_out_correct_posts(self):
+        """
+        It assumes that the two posts are linked in 'tag1'.
+        New visitor searches 'tag1', then the two posts are queried out in the
+        result.
+        """
+
+        post1 = Post(text='It is text1')
+        post1.save()
+
+        post2 = Post(text='It is text2')
+        post2.save()
+
+        tag1 = Tag(tag='tag1')
+        tag1.save()
+
+        tag1.post.add(post1)
+        tag1.post.add(post2)
+
+        connect('/')
+        result = search('tag1')
+        self.assertTrue(post1.text in result)
+        self.assertTrue(post2.text in result)
