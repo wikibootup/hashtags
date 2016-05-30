2da69af2aab6f7e00cf8c130ab1a947317875fa4
wikibootup <wikibootup@gmail.com>
2016-05-31 02:18:18 +0900

    delete FT home not have posts now

--- hashtags/functional_tests/tests.py
+++ hashtags/functional_tests/tests.py
@@ -22,27 +22,6 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.assertIn('Hashtags', self.browser.title)
 
-    def test_post_contains_correct_tags_and_text(self):
-        """
-        It assumes that the one post is shown to a new visitor. the visitor can
-        see the post which contains text and tags.
-        """
-        post = Post(text='It is text')
-        post.save()
-        tag1 = Tag(tag='tag1')
-        tag1.save()
-        tag2 = Tag(tag='tag2')
-        tag2.save()
-
-        tag1.post.add(post)
-        tag2.post.add(post)
-
-        self.browser.get(self.live_server_url)
-        body_text = self.browser.find_element_by_tag_name('body').text
-        self.assertIn('It is text', body_text)
-        self.assertIn('tag1', body_text)
-        self.assertIn('tag2', body_text)
-
     def test_home_has_search_tag(self):
         response = self.client.get('/')
         self.assertIsInstance(response.context['form'], SearchForm)
