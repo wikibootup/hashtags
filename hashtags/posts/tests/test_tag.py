3cf5733d65bd7bb61d0508287fb2bc670bfb16b7
wikibootup <wikibootup@gmail.com>
2016-06-21 00:40:01 +0900

    remove incorrect test

--- hashtags/posts/tests/test_tag.py
+++ hashtags/posts/tests/test_tag.py
@@ -4,12 +4,4 @@ from posts.models import Post, Tag
 
 
 class TagTest(TestCase):
-
-    def test_tag_itself_return_tag_name(self):
-        tag1 = Tag(tag='tag1')
-        tag1.save()
-        tag2 = Tag(tag='tag2')
-        tag2.save()
-
-        self.assertEqual('tag1', tag1)
-        self.assertEqual('tag2', tag2)
+    pass
