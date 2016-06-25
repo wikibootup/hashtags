e771d68aaf6ac31b69c3e63dc3870165d4fbbe10
wikibootup <wikibootup@gmail.com>
2016-06-25 17:05:59 +0900

    set autocomplete property in view(controller)

--- hashtags/hashtags/views.py
+++ hashtags/hashtags/views.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.core import serializers
 
 from posts.models import Post, Tag
 from posts.forms import SearchForm
@@ -23,11 +22,17 @@ def home(request):
             })
 
     if request.is_ajax() and request.GET.get('term'):
-        result = Tag.objects.filter(tag__istartswith=request.GET['term'])
-        result = serializers.serialize('json', result, indent=2)
+        result = []
+        tags = Tag.objects.filter(tag__istartswith=request.GET['term'])
+        for tag in tags:
+            tags_ = {}
+            tags_['id'] = tag.id
+            tags_['label'] = tag.tag
+            tags_['value'] = tag.tag
+            result.append(tags_)
 
         return HttpResponse(
-            json.dumps({'result': result}),
+            json.dumps(result),
             'application/json'
         )
     return render(request, 'home.html', {
