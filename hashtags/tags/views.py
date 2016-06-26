a49beb02b5044502394251dc0a5de004ad508713
wikibootup <wikibootup@gmail.com>
2016-06-26 21:10:58 +0900

    post_list template extended from base

--- hashtags/tags/views.py
+++ hashtags/tags/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from tags.forms import SearchForm
 
 
 def post_list(request, tag):
-    return render(request, 'tags/post_list.html')
+    return render(request, 'tags/post_list.html', {
+        'form': SearchForm()
+    })
