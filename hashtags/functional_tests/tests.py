db70fc00880a8908d0540a83a7b4c474358e76b4
wikibootup <wikibootup@gmail.com>
2016-06-29 17:05:21 +0900

    fix FT incorrect specific tag url

--- hashtags/functional_tests/tests.py
+++ hashtags/functional_tests/tests.py
@@ -42,9 +42,11 @@ class NewVisitorTest(LiveServerTestCase):
     def test_post_list_should_contain_correct_posts(self):
         for i in range(10):
             self.tag[0].post.add(self.post[i])
-        
-        url = "%s/%s/%s/" % (self.live_server_url, '/tags/', self.tag[0].tag)
+
+        url = "%s/%s/%s/" % (self.live_server_url, 'tags', self.tag[0].tag)
         self.browser.get(url)
-        items = self.browser.find_elements_by_class_name('post_list') 
-        for idx, item in enumerate(items):
-            self.assertEqual(self.post[i].text, item.text)
+
+        item_text = self.browser.find_element_by_class_name('post_list').text
+        item_count = item_text.count('It is text')
+        for idx in range(item_count):
+            self.assertIn(self.post[i].text, item_text)
