0d0ed1ba0937207731bebaea5161314309d31ca6
wikibootup <wikibootup@gmail.com>
2016-07-05 23:03:59 +0900

    reverse lazy url for cbv loginview

--- hashtags/accounts/views.py
+++ hashtags/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.edit import FormView
+from django.core.urlresolvers import reverse_lazy
 
 from accounts.forms import LoginForm
 
@@ -12,6 +13,7 @@ def login(request):
 class LoginView(FormView):
     template_name = 'accounts/login.html'
     form_class = LoginForm
+    success_url = reverse_lazy('home')
 
     def form_valid(self, form):
         return super(LoginView, self).form_valid(form)
